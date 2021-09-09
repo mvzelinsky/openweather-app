48c07a88441ae8c607e635ac83ef839cb5ab1bb3
MaZelinsky Maxim <mvzelinsky@yandex.ru>
2021-09-09 23:21:49 +0300

    feat: added weather icons

--- src/components/SidePanel.jsx
+++ src/components/SidePanel.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Form from './Form'
+import Weather from './Weather';
 import '../styles/SidePanel.css'
 
 function SidePanel() {
     return(
         <div className = "side-panel">
             <Form />
+            <Weather/>
         </div>
     );
 }
---
+++ src/components/Weather.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/weather.css'
+import 'weather-icons/css/weather-icons.css'
+
+function Weather() {
+    return(
+        <div className = "weather-container">
+            <i class="wi wi-cloud"></i>
+        </div>
+    );
+}
+
+export default Weather;
