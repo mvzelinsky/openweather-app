3614b34e860a039e675268622d9a7ce80c66a752
MaZelinsky Maxim <mvzelinsky@yandex.ru>
2021-09-10 12:28:34 +0300

    feat: added weather info

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -5,7 +5,16 @@ import 'weather-icons/css/weather-icons.css'
 function Weather() {
     return(
         <div className = "weather-container">
-            <i class="wi wi-cloud"></i>
+            <div className="weather-icon">
+                <i className="wi wi-fog"></i>
+            </div>
+            <div className="">
+                <div className="weather-info">
+                    <div className="city">Saint-Petersburg</div>
+                    <div className="degrees">15 &deg;C</div>
+                    <div className="today">Friday, 12:00</div>
+                </div>
+            </div>
         </div>
     );
 }
