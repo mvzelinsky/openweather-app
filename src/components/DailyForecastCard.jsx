0003797cc05c9b9cf615578725a0ce7301254a97
MaZelinsky Maxim <mvzelinsky@yandex.ru>
2021-09-11 20:08:38 +0300

    feat: put forecast info from API to cards

--- src/components/DailyForecastCard.jsx
+++ src/components/DailyForecastCard.jsx
@@ -1,35 +1,56 @@
 import React, { Component } from 'react';
 import '../styles/Forecast.css';
 
-export default class DailyForecastCard extends Component {
-  render() {
-    return (
-      <div>
-        <div className="daily">
-          <div className="date-block">
-            <div className="weekday">Today</div>
-            <div className="date">11.09</div>
+const DailyForecastCard = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <div className="daily">
+        <div className="date-block">
+          <div className="weekday">{getWeekDay(props.dt)}</div>
+          <div className="date">{getCurrentTimeFromStamp(props.dt)}</div>
+        </div>
+        <div className="temp">
+          <div className="icon">
+            <i className={`wi wi-owm-${props.weather[0].id}`}></i>
           </div>
-          <div className="temp">
-            <div className="icon">
-              <i className="wi wi-owm-501"></i>
-            </div>
-            <div className="day-temp">
-              18
-              <i className="wi wi-celsius"></i>
-            </div>
-            <div className="night-temp">
-              14
-              <i className="wi wi-celsius"></i>
-            </div>
+          <div className="day-temp">
+            {calcTemp(props.temp.max)}
+            <i className="wi wi-celsius"></i>
           </div>
-          <div className="weather-description daily-text">Broken clouds</div>
-          <div className="daily-humidity">
-            <i className="wi wi-humidity mr10"></i>
-            <span className="daily-text">76</span>
+          <div className="night-temp">
+            {calcTemp(props.temp.min)}
+            <i className="wi wi-celsius"></i>
           </div>
         </div>
+        <div className="weather-description daily-text">{props.weather[0].description}</div>
+        <div className="daily-humidity">
+          <i className="wi wi-humidity mr10"></i>
+          <span className="daily-text">{props.humidity}</span>
+        </div>
       </div>
-    );
+    </div>
+  );
+
+  function calcTemp(temp) {
+    return Math.floor(temp - 273.15);
   }
-}
+
+  function getCurrentTimeFromStamp(timestamp) {
+    let d = new Date(timestamp * 1000);
+    let day = d.getDate();
+    let month = d.getMonth();
+    if (month / 10 < 1) {
+      month = '0' + month.toString();
+    }
+    return day + '.' + month;
+  }
+
+  function getWeekDay(timestamp) {
+    let week = ['Sunday', 'Monday', 'Tuesday', 'Wednsday', 'Thursday', 'Friday', 'Saturday'];
+    let d = new Date(timestamp * 1000);
+    return week[d.getDay()];
+  }
+};
+
+export default DailyForecastCard;
